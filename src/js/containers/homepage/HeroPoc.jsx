8cac478420aca4be20c9a61c86064ce9ff11c868
Brett Varney <brett.varney@kc.frb.org>
2022-02-24 15:43:53 -0600

    8472 both slides cycling

--- src/js/containers/homepage/HeroPoc.jsx
+++ src/js/containers/homepage/HeroPoc.jsx
@@ -9,15 +9,28 @@ const HeroPoc = () => {
 
     const downloadRef = useRef(null);
     const trackRef = useRef(null);
+    const receivesRef = useRef(null);
+    const givesRef = useRef(null);
 
     const switchLeft = (show, hide) => {
         hide.classList.add("hero_fadeOut");
-        hide.classList.remove("hero_show");
-        hide.classList.remove("hero_slideRight");
+        hide.classList.remove("hero_hideLeft");
+        hide.classList.remove("hero_slide");
         show.classList.remove("hero_fadeOut");
-        show.classList.add("hero_show");
+        show.classList.add("hero_hideLeft");
         setTimeout(() => {
-            show.classList.add("hero_slideRight"); // must be slightly after out of view
+            show.classList.add("hero_slide"); // must be slightly after out of view
+        }, 1);
+    };
+
+    const switchRight = (show, hide) => {
+        hide.classList.add("hero_fadeOut");
+        hide.classList.remove("hero_hideRight");
+        hide.classList.remove("hero_slide");
+        show.classList.remove("hero_fadeOut");
+        show.classList.add("hero_hideRight");
+        setTimeout(() => {
+            show.classList.add("hero_slide"); // must be slightly after out of view
         }, 1);
     };
 
@@ -25,6 +38,9 @@ const HeroPoc = () => {
     setTimeout(() => {
         switchLeft(trackRef.current, downloadRef.current);
     }, 2000);
+    setTimeout(() => {
+        switchRight(givesRef.current, receivesRef.current);
+    }, 6000);
 
     // timed from then on
     setInterval(() => {
@@ -34,7 +50,13 @@ const HeroPoc = () => {
         setTimeout(() => {
             switchLeft(trackRef.current, downloadRef.current);
         }, 6000);
-    }, 10000);
+        setTimeout(() => {
+            switchRight(givesRef.current, receivesRef.current);
+        }, 8000);
+        setTimeout(() => {
+            switchRight(receivesRef.current, givesRef.current);
+        }, 10000);
+    }, 12000);
 
     return (
         <section className="wrapper">
@@ -47,9 +69,11 @@ const HeroPoc = () => {
                 </div>
                 <span className="staticBlock">government spending</span>
 
-                <div class="slideLeft">
-                    <span>by who receives money</span>
-                    <span>by who gives money</span>
+                <div class="rightWords">
+                    <span ref={receivesRef}>by who receives money</span>
+                    <span ref={givesRef} className="hero_fadeOut">
+                        by who gives money
+                    </span>
                 </div>
             </h2>
         </section>
